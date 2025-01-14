b70cc596d0bf07ab40cad8a200be1677f9a8fed5
David Odari <david.kiribwa@blacklane.com>
2025-01-14 10:04:18 +0100

    fix compile and target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,12 +110,12 @@ fun setupAndroidReporting() {
 
 android {
     namespace = "com.github.odaridavid.weatherapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.github.odaridavid.weatherapp"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
